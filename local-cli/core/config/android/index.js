ab467c2e832680e13cea623484f56ff6ede01e13
Kishan Bharda <39869106+kishanbharda@users.noreply.github.com>
2019-08-13 17:19:27 +0530

    Update index.js

--- local-cli/core/config/android/index.js
+++ local-cli/core/config/android/index.js
@@ -30,8 +30,7 @@ exports.projectConfig = function projectConfigAndroid(folder, userConfig) {
   const manifest = readManifest(manifestPath);
 
   const packageName = userConfig.packageName || getPackageName(manifest);
-  const packageFolder = userConfig.packageFolder ||
-//     packageName.replace(/\./g, path.sep);
+  const packageFolder = userConfig.packageFolder || packageName.replace(/\./g, path.sep);
 
   const mainFilePath = path.join(
     sourceDir,
